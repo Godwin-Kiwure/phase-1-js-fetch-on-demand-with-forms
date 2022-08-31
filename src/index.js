473059b5f522724d71c0167959cd5f7f68326780
Kiwg <gkiwure21@gmail.com>
2022-08-31 12:13:18 +0300

    Completed asssignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-const init = () => {
-  
+const url = "http://localhost:3000/movies";
+document.addEventListener("DOMContentLoaded", () => {
+    fetch();
+    FormData();
+});
+
+function fetch() {
+    fetch(url)
+    .then((resp) => resp.json())
+    .then((godwin) => console.log(godwin));
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+function FormData() {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", () => {});
+}
